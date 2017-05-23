ef033be28d025d3c434161125f353b650c060ec0
root@ctdb <root@ctcdb.chicagotopcondos.com>
2017-05-23 05:06:10 +0000

    capture output of logrot.sh for sns

--- db-nightly.sh
+++ db-nightly.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-$(basename $0)/db-tools/logrot.sh
+LOGROT=$(basename $0)/db-tools/logrot.sh
 
 TARGET="s3://ctcdb.chicagotopcondos.com/mysql-backup/mysql-backup.$(date '+%Y-%m-%d').sql.gz"
 
@@ -8,6 +8,7 @@ aws sns publish \
 	--region us-east-2 \
 	--topic-arn arn:aws:sns:us-east-2:984073016564:cron \
 	--message file://<(sh -c "exec 2>&1; \
+				 $LOGROT; \
 				 mysqldump -A -R --single-transaction --master-data=2 --max_allowed_packet=64M |\
 				 dd |\
 				 gzip |\
