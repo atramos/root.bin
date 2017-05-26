2249b509be9416ca5be3d93545ab75533e28e6dc
root@ctdb <root@ctcdb.chicagotopcondos.com>
2017-05-26 04:34:00 +0000

    db-nightly.sh fixes

--- db-nightly.sh
+++ db-nightly.sh
@@ -2,14 +2,15 @@
 
 export AWS_DEFAULT_REGION=us-east-2
 
+HOST=$(hostname -s)
 LOGROT=$(dirname $0)/db-tools/logrot.sh
-
-TARGET="s3://ctcdb.chicagotopcondos.com/mysql-backup/mysql-backup.$(date '+%Y-%m-%d').sql.gz"
+BUCKET=$HOST.chicagotopcondos.com
+TARGET="s3://$BUCKET/mysql-backup/mysql-backup.$(date '+%Y-%m-%d').sql.gz"
 
 aws sns publish \
-	--topic-arn arn:aws:sns:us-east-2:984073016564:cron \
+	--topic-arn arn:aws:sns:us-east-2:984073016564:sysadmin \
 	--message file://<(sh -c "exec 2>&1; \
-				 $LOGROT; \
+				 $LOGROT $BUCKET; \
 				 mysqldump -A -R --single-transaction --master-data=2 --max_allowed_packet=64M |\
 				 dd |\
 				 gzip |\
